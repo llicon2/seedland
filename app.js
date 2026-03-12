21e79f81cec4a8095d08c64dd611aa176af72918
Christian <christianmh12@gmail.com>
2026-03-12 18:41:22 -0300

    supabase conectado

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-let users = JSON.parse(localStorage.getItem("seedland_users")) || [];
+const supabaseUrl = "https://lvtpsqoqywoxrvbqfycd.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dHBzcW9xeXdveHJ2YnFmeWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDg1ODEsImV4cCI6MjA4ODkyNDU4MX0.AiHAyOS3zZrX3R1gPhp6GlXDOI5RO6eEF1lnLv0tnCU"
 
-function preregister() {
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
-    const tg = window.Telegram.WebApp;
-    const user = tg.initDataUnsafe.user;
+async function preregister() {
+
+    const tg = window.Telegram.WebApp
+    const user = tg.initDataUnsafe.user
 
     if (!user) {
-        alert("Abre esto desde Telegram");
-        return;
+        alert("Abre esto desde Telegram")
+        return
     }
 
-    const exists = users.find(u => u.id === user.id);
+    const { data } = await supabase
+        .from("players")
+        .select("*")
+        .eq("id", user.id)
 
-    if (!exists) {
-        users.push({
-            id: user.id,
-            name: user.first_name
-        });
+    if (data.length === 0) {
 
-        localStorage.setItem("seedland_users", JSON.stringify(users));
+        await supabase
+            .from("players")
+            .insert([
+                {
+                    id: user.id,
+                    name: user.first_name
+                }
+            ])
 
-        document.getElementById("status").innerText = "✅ Ya estás preregistrado!";
-    } else {
-        document.getElementById("status").innerText = "⚠️ Ya estabas preregistrado.";
+        document.getElementById("status").innerText = "✅ Ya estás preregistrado!"
+    }
+    else {
+        document.getElementById("status").innerText = "⚠️ Ya estabas preregistrado."
     }
 
-    document.getElementById("count").innerText = users.length;
+    updateCount()
 }
+
+async function updateCount() {
+
+    const { count } = await supabase
+        .from("players")
+        .select("*", { count: "exact" })
+
+    document.getElementById("count").innerText = count
+}
+
+updateCount()
