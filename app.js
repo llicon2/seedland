d1923b4ac7e939dc977739a5e5bd6025dec0c59b
Christian <christianmh12@gmail.com>
2026-03-12 21:23:15 -0300

    fix preregistro

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
-const supabaseUrl = "https://lvtpsqoqywoxrvbqfycd.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dHBzcW9xeXdveHJ2YnFmeWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDg1ODEsImV4cCI6MjA4ODkyNDU4MX0.AiHAyOS3zZrX3R1gPhp6GlXDOI5RO6eEF1lnLv0tnCU"
+const SUPABASE_URL = "https://lvtpsqoqywoxrvbqfycd.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dHBzcW9xeXdveHJ2YnFmeWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDg1ODEsImV4cCI6MjA4ODkyNDU4MX0.AiHAyOS3zZrX3R1gPhp6GlXDOI5RO6eEF1lnLv0tnCU"
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
-async function preregister() {
+async function preregister(){
 
-    const tg = window.Telegram.WebApp
-    const user = tg.initDataUnsafe.user
+const tg = window.Telegram.WebApp
+const user = tg.initDataUnsafe.user
 
-    if (!user) {
-        alert("Abre esto desde Telegram")
-        return
-    }
-
-    const { data } = await supabase
-        .from("players")
-        .select("*")
-        .eq("id", user.id)
-
-    if (data.length === 0) {
-
-        await supabase
-            .from("players")
-            .insert([
-                {
-                    id: user.id,
-                    name: user.first_name
-                }
-            ])
-
-        document.getElementById("status").innerText = "✅ Ya estás preregistrado!"
-    }
-    else {
-        document.getElementById("status").innerText = "⚠️ Ya estabas preregistrado."
-    }
-
-    updateCount()
+if(!user){
+document.getElementById("status").innerText = "❌ Error cargando usuario de Telegram"
+return
 }
 
-async function updateCount() {
+const { error } = await supabase
+.from("players")
+.insert([
+{
+telegram_id: user.id
+}
+])
 
-    const { count } = await supabase
-        .from("players")
-        .select("*", { count: "exact" })
-
-    document.getElementById("count").innerText = count
+if(error){
+document.getElementById("status").innerText = "⚠️ Ya estabas preregistrado."
+}else{
+document.getElementById("status").innerText = "✅ Ya estás preregistrado!"
+loadCount()
 }
 
-updateCount()
+}
+
+async function loadCount(){
+
+const { data } = await supabase
+.from("players")
+.select("*")
+
+if(data){
+document.getElementById("count").innerText = data.length
+}
+
+}
+
+loadCount()
