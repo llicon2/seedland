f2ddcabd2ac222be04bfa332a0b7dd5af4e26a7e
Christian <christianmh12@gmail.com>
2026-03-16 19:41:21 -0300

    fix preregistro

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 console.log("APP INICIADA")
-console.log(window.Telegram)
 
 const SUPABASE_URL = "https://lvtpsqoqywoxrvbqfycd.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dHBzcW9xeXdveHJ2YnFmeWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDg1ODEsImV4cCI6MjA4ODkyNDU4MX0.AiHAyOS3zZrX3R1gPhp6GlXDOI5RO6eEF1lnLv0tnCU"
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
-window.preregister = async function(){
+window.preregister = async function () {
 
 const tg = window.Telegram.WebApp
+tg.ready()
+
 const user = tg.initDataUnsafe.user
 
 if(!user){
-document.getElementById("status").innerText = "❌ Error cargando usuario de Telegram"
+document.getElementById("status").innerText =
+"❌ Error cargando usuario de Telegram"
 return
 }
 
@@ -25,9 +27,11 @@ telegram_id: user.id
 ])
 
 if(error){
-document.getElementById("status").innerText = "⚠️ Ya estabas preregistrado."
+document.getElementById("status").innerText =
+"⚠️ Ya estabas preregistrado"
 }else{
-document.getElementById("status").innerText = "✅ Ya estás preregistrado!"
+document.getElementById("status").innerText =
+"✅ Preregistro completado"
 loadCount()
 }
 
@@ -35,13 +39,11 @@ loadCount()
 
 async function loadCount(){
 
-const { data } = await supabase
+const { count } = await supabase
 .from("players")
-.select("*")
+.select("*", { count: "exact", head: true })
 
-if(data){
-document.getElementById("count").innerText = data.length
-}
+document.getElementById("count").innerText = count
 
 }
 
